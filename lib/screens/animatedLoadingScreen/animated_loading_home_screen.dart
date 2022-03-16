58d549e210e895595d25fb788ecf88764bd99556
Mano Vikram <manovik18@gmail.com>
2022-03-16 18:47:43 +0530

    Starting Animated Loading Screen app

---
+++ lib/screens/animatedLoadingScreen/animated_loading_home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AnimatedLoadingHomeScreen extends StatelessWidget {
+  const AnimatedLoadingHomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
